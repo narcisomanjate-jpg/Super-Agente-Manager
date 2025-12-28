a377dc47e9e32f92c4e58359e475005aeace6ba8
narcisomanjate-jpg <narciso.manjate@gmail.com>
2025-12-28 14:44:55 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+// Service Worker vazio para permitir a instalação do PWA.
+// Pode adicionar lógica de cache mais complexa aqui mais tarde.
+self.addEventListener('fetch', function(event) {
+  // Deixamos vazio por agora para o setup mínimo.
+});
