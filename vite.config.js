ef3fc95fc59df77e61f46b72db9400d0de2e7278
narcisomanjate-jpg <narciso.manjate@gmail.com>
2025-12-28 17:59:22 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// vitejs.dev
 export default defineConfig({
   plugins: [react()],
-  base: './', // Define o caminho base como relativo
+  base: './', // Define o caminho base como relativo para funcionar no GitHub Pages
 });
